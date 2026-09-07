c3ae86e2a78c89b0a54103607344fb39eb7b855f
bluedot522 <katwengercat@gmail.com>
2026-09-07 18:36:30 -0400

    Add files via upload

---
+++ SortAndDisplayWords.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+using System.Globalization;
+using System.ComponentModel.DataAnnotations;
+class SortWords
+{
+    static void Main()
+    {
+        WriteLine("Enter total number of words");
+        int length = Convert.ToInt32(ReadLine());
+        WriteLine("Enter words");
+        string[] wordArray = new string[length];
+        int i = 0;
+        while (i < length)
+        {
+            string newInput = ReadLine();
+            wordArray[i] = newInput;
+            i += 1;
+        }
+
+
+        SortAndDisplayWords(wordArray);
+
+    }
+
+    static void SortAndDisplayWords(string[] words)
+    {
+        Array.Sort(words);
+
+        foreach (string word in words)
+        {
+            Write("{0} ", word);
+
+        }
+
+    }
+}
